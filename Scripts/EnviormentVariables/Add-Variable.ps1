bd42f0adb6df7b6d3afaba7830db2ec8c223ba07
Maxwell Carlson <carlsonamax@gmail.com>
2024-07-10 22:12:03 -0700

    changed some alias to functions where required.

--- Scripts/EnviormentVariables/Add-Variable.ps1
+++ Scripts/EnviormentVariables/Add-Variable.ps1
@@ -1,4 +1,6 @@
 # Define the function to add a variable permanently
+# Does so by copying the Variable to the ProfileCUCH.ps1 file
+# Then running the CopyProfileToActive.ps1 script to copy the ProfileCUCH.ps1 to the active profile
 function Add-PersistentVariable {
     param (
         [Parameter(Mandatory = $true)]
@@ -16,7 +18,7 @@ function Add-PersistentVariable {
         Set-Variable -Name $VariableName -Value $Value
 
         # Add the variable to the PowerShell profile
-        $profilePath = $PROFILE
+        $profilePath = "$PSScriptRoot\..\Profiles\ProfileCUCH.ps1"
 
         # Check if the profile file exists
         if (-not (Test-Path $profilePath)) {
@@ -27,9 +29,12 @@ function Add-PersistentVariable {
         # Append the variable definition to the profile file
         Add-Content -Path $profilePath -Value "`n`$global:$VariableName = '$Value'"
 
+        $addToProfilePath = "$PSScriptRoot\..\Profiles\CopyProfileToActive.ps1"
+        & $addToProfilePath 
+
         Write-Host "The variable '$VariableName' has been added and will be available in future sessions."
     }
 }
 
 # Example usage
-# Add-PersistentVariable -VariableName "MyVar" -Value "MyValue"
+Add-PersistentVariable -VariableName $args[0] -Value $args[1]
